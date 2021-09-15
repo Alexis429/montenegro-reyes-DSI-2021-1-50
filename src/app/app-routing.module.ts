d9837414e1a06068c8265dc0cfd4dd7a05436978
1NE0 <juan.ortiz.alzate@correounivalle.edu.co>
2021-09-15 18:38:00 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PanelAdminComponent } from './components/panel-admin/panel-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'login', component: PanelAdminComponent},
+  
+
+  {path: '**', redirectTo:'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
